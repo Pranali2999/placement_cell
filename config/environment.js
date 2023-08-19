c58353e20c251f503cad38861b543a20fdaa2a48
Pranali2999 <78157763+Pranali2999@users.noreply.github.com>
2023-08-19 19:40:49 +0530

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const rfs = require('rotating-file-stream');
 const path = require('path');
 
-const logDirectory = path.join(__dirname,'../production_logs');
+const logDirectory = path.join(__dirname, '../production_logs');
 fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
 
 const accessLogStream = rfs.createStream('access.log', {
@@ -11,27 +11,27 @@ const accessLogStream = rfs.createStream('access.log', {
 });
 
 const development = {
-    name: 'development',
-    asset_path: './assets',
-    session_cookie_key: 'blahsomething',
-    db: 'mp_development',
-    morgan:{
-      mode: 'dev',
-      options: {stream: accessLogStream}
-    },
-    MONGO_URL: 'mongodb://localhost:27017/mp_development' 
-  }
+  name: 'development',
+  asset_path: './assets',
+  session_cookie_key: 'blahsomething',
+  db: 'mp_development',
+  morgan: {
+    mode: 'dev',
+    options: { stream: accessLogStream }
+  },
+  MONGO_URL: 'mongodb://localhost:27017/mp_development'
+};
 
 const production = {
-    name: 'production',
-    asset_path: process.env.CODIAL_ASSET_PATH,
-    session_cookie_key: process.env.CODIAL_SESSION_COOKIE_KEY,
-    db: process.env.CODIAL_DB,
-    morgan:{
-      mode: 'combined',
-      options: {stream: accessLogStream}
-    },
-    MONGO_URL: process.env.MONGO_URL
-}
+  name: 'production',
+  asset_path: process.env.CODIAL_ASSET_PATH,
+  session_cookie_key: process.env.CODIAL_SESSION_COOKIE_KEY,
+  db: process.env.CODIAL_DB,
+  morgan: {
+    mode: 'combined',
+    options: { stream: accessLogStream }
+  },
+  MONGO_URL: 'mongodb+srv://Pranali:ucsAdLmhDjhdgu22@cluster0.lqn3xio.mongodb.net/?retryWrites=true&w=majority'
+};
 
 module.exports = eval(process.env.CODIAL_ENVIRONMENT) === undefined ? development : eval(process.env.CODIAL_ENVIRONMENT);
