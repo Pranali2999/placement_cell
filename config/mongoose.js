0b151a38ad9eb9a54884a783601224c027ac5866
Pranali2999 <78157763+Pranali2999@users.noreply.github.com>
2023-08-19 19:32:43 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const config = require('./environment'); // Update the path
 
 async function main() {
+    console.log(config.MONGO_URL);
     await mongoose.connect(config.MONGO_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
