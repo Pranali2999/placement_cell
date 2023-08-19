16ed62dcf40129587ea8e9eb260c4f205dfd9749
Pranali2999 <78157763+Pranali2999@users.noreply.github.com>
2023-08-19 20:26:00 +0530

    Update jobsController.js

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,14 +1,15 @@
+const fetch = require('node-fetch');
+
 module.exports.jobPage = async function (req, res) {
     try {
-      const response = await fetch('https://remotive.com/api/remote-jobs');
-      const jobsData = await response.json();
-      return res.render('job', {
-        title: 'Placement Cell',
-        body: jobsData.jobs,
-      });
+        const response = await fetch('https://remotive.com/api/remote-jobs');
+        const jobsData = await response.json();
+        return res.render('job', {
+            title: 'Placement Cell',
+            body: jobsData.jobs,
+        });
     } catch (error) {
-      console.error('Error fetching jobs:', error);
-      return res.render('job', { body: [] });
+        console.error('Error fetching jobs:', error);
+        return res.render('job', { body: [] });
     }
-  };
-  
+};
