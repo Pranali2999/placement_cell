0806c60abf229d55ecae9dd68d927ae5f375c424
Pranali2999 <78157763+Pranali2999@users.noreply.github.com>
2023-08-19 17:59:19 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const env= require('./environment')
 
-mongoose.connect(`mongodb://127.0.0.1:27017/${env.db}`);
+//mongoose.connect(`mongodb://127.0.0.1:27017/${env.db}`);
+mongoose.connect(`mongodb+srv://Pranali:ucsAdLmhDjhdgu22@cluster0.lqn3xio.mongodb.net/`);
 
 const db = mongoose.connection;
 
