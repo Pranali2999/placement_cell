aa629dbe36c5ef6e4a4f54afcdf763f0be9910fd
Pranali2999 <78157763+Pranali2999@users.noreply.github.com>
2023-08-20 19:32:05 +0530

    Update studentController.js

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -36,45 +36,7 @@ module.exports.createStudent = async function (req, res) {
   }
 };
 
-// edit student
-// module.exports.deleteStudent = async function (req, res) {
-//   const { id } = req.params;
-//   try {
-//     const student = await Student.findById(id);
-
-//     if (student && student.interviews.length > 0) {
-//       for (let item of student.interviews) {
-//         const company = await Company.findOne({ name: item.company });
-//         if (company) {
-//           for (let i = 0; i < company.students.length; i++) {
-//             if (company.students[i].student.toString() === id) {
-//               company.students.splice(i, 1);
-//               await company.save();
-//               break;
-//             }
-//           }
-//         }
-//       }
-//     }
-//     if(req.xhr){
-//       return res.status(200).json({
-//         data:{
-//           success: true,
-//           student_id: id
-//         },
-//         message: 'student deleted'
-//       })
-//     }
-
-//     await Student.findByIdAndDelete(id);
-//     res.redirect('back');
-//   } catch (error) {
-//     console.log('Error in deleting student');
-//     return res.redirect('back');
-//   }
-// };
-
-
+//delete student
 module.exports.deleteStudent = async function (req, res) {
   const { id } = req.params;
   try {
@@ -125,3 +87,49 @@ module.exports.deleteStudent = async function (req, res) {
     }
   }
 };
+
+//update status
+module.exports.updateStudentPlacement = async function (req, res) {
+  const studentId = req.params.id;
+  const { companyResult } = req.body;
+
+  try {
+    // Find the student by ID
+    const student = await Student.findById(studentId);
+
+    if (!student) {
+      return res.status(404).json({
+        success: false,
+        message: 'Student not found',
+      });
+    }
+
+    // Update the placement status
+    student.placement = companyResult;
+
+    // Save the updated student
+    await student.save();
+
+    if (req.xhr) {
+      return res.status(200).json({
+        success: true,
+        student_id: studentId,
+        message: 'Placement status updated',
+      });
+    } else {
+      res.redirect('back');
+    }
+  } catch (error) {
+    console.error('Error updating placement status:', error);
+    if (req.xhr) {
+      return res.status(500).json({
+        success: false,
+        message: 'Error updating placement status',
+      });
+    } else {
+      return res.redirect('back');
+    }
+  }
+};
+
+
